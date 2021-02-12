bc23edd7bbab6679034a4c3c6e521f3219779840
Natalia Davydova <davydovanatalia063@gmail.com>
2021-02-11 20:37:05 -0800

    add data attribute novalidate to index.html, add name and value to radio buttons, add function gatherCardData and gatherdata from radio buttons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+const answers = {
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null
+}
+
 const btnNext = document.querySelectorAll('[data-nav="next"]');
 btnNext.forEach(button => {
   button.addEventListener('click', function () {
     const thisCard = this.closest('[data-card]');
-    navigate('next', thisCard);
+
+    if (thisCard.dataset.validate == 'novalidate') {
+      console.log('novalidate');
+      navigate('next', thisCard);
+    } else {
+      console.log('validate');
+      navigate('next', thisCard);
+    }
   });
 });
 
@@ -28,4 +43,30 @@ function navigate(direction, thisCard) {
 
   thisCard.classList.add('hidden');
   document.querySelector(`[data-card="${nextCard}"]`).classList.remove('hidden');
+}
+
+function gatherCardData(number) {
+  let question,
+    result = [];
+
+  let currentCard = document.querySelector(`[data-card="${number}"]`);
+
+  question = currentCard.querySelector('[data-question]').innerText;
+
+  let radioValues = currentCard.querySelectorAll('[type="radio"]');
+  radioValues.forEach(item => {
+
+    if (item.checked) {
+      result.push({
+        name: item.name,
+        value: item.value
+      })
+    }
+  })
+
+  let data = {
+    question: question,
+    answer: result
+  }
+  return data;
 }
